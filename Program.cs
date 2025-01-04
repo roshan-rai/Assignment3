f58d59aa75b091c160eda15c090522801e977a48
Roshan Rai <roshan.rai@ou.edu>
2025-01-04 15:01:14 -0600

    Asssignment 3 set uo

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World !!!");
+
+        // Generate numbers
+        int[] numbers = GenerateNumbers();
+
+        // Reverse the array
+        Reverse(numbers);
+
+        // Print the reversed array
+        PrintNumbers(numbers);
+    }
+
+    // Method to generate an array of numbers
+    public static int[] GenerateNumbers()
+    {
+        return null;
+    }
+
+    // Method to reverse the array in place
+    public static void Reverse(int[] listNumber)
+    {
+        
+    }
+
+    // Method to print the array
+    public static void PrintNumbers(int[] arrayNumber)
+    {
+       
+    }
+}
